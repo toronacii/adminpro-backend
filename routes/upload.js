2c8eaa2492de3c719d0e7ef012945614165d6c24
Jose Toro <jose@leadbox.ca>
2018-05-16 09:10:58 -0300

    Upload files

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const fs = require('fs');
 const fileUpload = require('express-fileupload');
 const HttpStatus = require('http-status-codes');
+
+const User = require('../models/user');
+const Doctor = require('../models/doctor');
+const Hospital = require('../models/hospital');
 
 var app = express();
 
@@ -51,8 +56,49 @@ app.put('/:resource/:id', ({ files, params }, res) => {
                 })
         }
 
-        res.status(200).send("ok");
+        uploadByResource(params.resource, params.id, path, res);
     });
 });
+
+function uploadByResource(resource, id, path, res) {
+    let Model;
+    switch(resource) {
+        case 'user': Model = User; break;
+        case 'hospital': Model = Hospital; break;
+        case 'doctor': Model = Doctor; break;
+    }
+
+    Model.findById(id, (err, model) => {
+        if (err) {
+            return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+                message: `Error finding ${ resource } with id ${ id }`,
+                errors: err
+            })
+        }
+
+        if (!model) {
+            return res.status(HttpStatus.NOT_FOUND).json({
+                message: `The model with id ${ id } does not exists`,
+                errors: err
+            })
+        }
+
+        if (model.avatar && fs.existsSync(model.avatar)) {
+            fs.unlink(model.avatar);
+        }
+
+        model.avatar = path;
+
+        model.save((err, modelUpdated) => {
+            if (err) {
+                return res.status(HttpStatus.BAD_REQUEST).json({
+                    message: `Error relating image to ${ resource }`,
+                    errors: err
+                })
+            }
+            return res.json(modelUpdated);
+        })
+    })
+}
 
 module.exports = app;
